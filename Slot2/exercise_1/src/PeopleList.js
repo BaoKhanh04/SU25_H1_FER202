df4c04aef864b3194d5c6ca9658d9543d3723ea9
BaoKhanh04 <baokhanh652210@gmail.com>
2025-05-16 22:51:00 +0700

    add slot 2

---
+++ Slot2/exercise_1/src/PeopleList.js
@@ -0,0 +1,22 @@
+// src/PeopleList.js
+function PeopleList() {
+  const people = [
+    { name: "Alice", age: 25, occupation: "Engineer" },
+    { name: "Bob", age: 19, occupation: "Student" },
+    { name: "Charlie", age: 32, occupation: "Teacher" },
+  ];
+
+  return (
+    <div>
+      <ul>
+        {people.map((person, index) => (
+          <li key={index}>
+            {person.name} - {person.age} - {person.occupation}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PeopleList;
