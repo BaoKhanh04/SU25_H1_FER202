d23bb2a4b11b77276b172a8d6a751968f14495ae
BaoKhanh04 <baokhanh652210@gmail.com>
2025-06-13 14:13:52 +0700

    ipm EX_usestate_hook

---
+++ Slot10/usestate_hook/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Example1 from './components/Example1';
+import Example2 from './components/Example2';
+import Example3 from './components/Example3';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Example1/>
+      <br></br>
+      <Example2/>
+      <Example3/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
